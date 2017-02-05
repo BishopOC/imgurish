5196ed85e32160a37e6007c69629fc59599f46c2
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-02-04 21:35:57 -0500

    Justin said not to work on this over the weekend...I couldnt help myself

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+var imageApp = {
+  images: [],
+  init: function(){
+    imageApp.cacheDom();
+    imageApp.addEventListeners();
+    imageApp.render();
+  },
+  cacheDom: function(){
+    imageApp.createButton = document.querySelector('#create');
+    imageApp.imageInput = document.querySelector('#image-url');
+    imageApp.list = document.querySelector('#list');
+  },
+  render: function (){
+    console.log(imageApp.images);
+    var listItemsFromImages = imageApp.images
+                                      .map(function(imageurl){
+                                        return `<li><img src=${imageurl.image}/></li>`;
+                                      })
+                                      .join('');
+    imageApp.list.innerHTML = listItemsFromImages;
+  },
+  addEventListeners: function(){
+    imageApp.createButton.addEventListener('click', imageApp.addImage);
+  },
+  addImage: function(){
+    var image = imageApp.imageInput.value;
+    var newImage = createImage(image);
+    imageApp.images.push(newImage);
+    imageApp.imageInput.value = '';
+    imageApp.render();
+  }
+};
+imageApp.init();
---
+++ public/js/image.js
@@ -0,0 +1,6 @@
+function createImage(image){
+  var imageurl = {};
+  imageurl.image = image;
+  return imageurl;
+
+}
